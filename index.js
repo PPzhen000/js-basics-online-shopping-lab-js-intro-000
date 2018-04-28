6eadfb7dcf5c8ab23f15a68ac0d4841f6ce4c350
Ariel Zhen <zhenpingping@live.com>
2018-04-28 00:26:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,10 +52,11 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-    else{
-      return "That item is not in your cart.";
-    }
+    // else{
+    //   return "That item is not in your cart.";
+    // }
   }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
